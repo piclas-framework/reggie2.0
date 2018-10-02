5aba88be4cfd300d955c3d6205a160af49fae93c
Stephen Copplestone <copplestone@iag.uni-stuttgart.de>
2018-10-02 14:22:31 +0200

    repas: fix path

--- repas/settings.py
+++ repas/settings.py
@@ -1,9 +1,10 @@
 # settings.py
 import os
+import sys
 
 def init():
     global absolute_reggie_path
-    absolute_reggie_path='/home/stephen/Flexi/reggie/'
+    absolute_reggie_path=os.path.abspath(os.path.join(__file__ ,"../.."))
     if not os.path.exists(absolute_reggie_path) :
         print "Reggie repository not found under: '%s'" % absolute_reggie_path
         exit(1)
