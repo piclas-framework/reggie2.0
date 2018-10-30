53b8ff4bbb865d779ac367a4dd04ed37f4b89b18
Stephen Copplestone <copplestone@iag.uni-stuttgart.de>
2018-10-30 13:52:22 +0100

    added licence GNU GPL v3

---
+++ CONTRIBUTORS.md
@@ -0,0 +1,9 @@
+# List of Contributors
+
+This is a (possibly incomplete) list of the people who contributed to reggie2.0.
+
+* Stephen Copplestone
+* Matthias Sonntag
+* Fabian Foell
+* Christoph Mueller
+* Jakob Duerrwaechter
--- repas/repas.py
+++ repas/repas.py
@@ -1,3 +1,15 @@
+#==================================================================================================================================
+# Copyright (c) 2017 - 2018 Stephen Copplestone
+#
+# This file is part of reggie2.0 (gitlab.com/reggie2.0/reggie2.0). reggie2.0 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3
+# of the License, or (at your option) any later version.
+#
+# reggie2.0 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License v3.0 for more details.
+#
+# You should have received a copy of the GNU General Public License along with reggie2.0. If not, see <http://www.gnu.org/licenses/>.
+#==================================================================================================================================
 # import general functions
 import os
 import fileinput
