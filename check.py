c2e5e446153a66ce0199f86f4dad824db3274b7a
Stephen Copplestone <stephen@copplestone.de>
2019-08-30 16:26:47 +0200

    added try-except statements to number of elem check when running with MPI + info if number of MPI ranks is changed automatically

--- check.py
+++ check.py
@@ -373,16 +373,28 @@ class Run(OutputDirectory, ExternalCommand) :
         # create parameter file with one set of combinations
         combinations.writeCombinationsToFile(self.parameters, self.parameter_path)
 
-        # check MPI threads for mpirun
+        # Get MPI threads for mpirun
         MPIthreads = command_line.parameters.get('MPI')
 
         # Safety check: Get the mesh file, then extract the number of elements using h5py, limit the number of mpithreads
         if h5py_module_loaded:
-            MeshFileName = combinations.readValueFromFile(self.parameter_path,'MeshFile')
-            with h5py.File(os.path.join(self.target_directory,MeshFileName), 'r') as MeshFile:
-                nElems = MeshFile.attrs[u'nElems']
-                if MPIthreads:
-                    MPIthreads = str(min(int(nElems[0]),int(MPIthreads)))
+            try:
+                # Get the mesh file
+                MeshFileName = combinations.readValueFromFile(self.parameter_path,'MeshFile')
+
+                # Extract the number of elements using h5py
+                with h5py.File(os.path.join(self.target_directory,MeshFileName), 'r') as MeshFile:
+                    nElems = MeshFile.attrs[u'nElems']
+
+                    # Limit the number of mpithreads
+                    if MPIthreads:
+                        if int(MPIthreads) > int(nElems[0]) :
+                            s = tools.yellow("Reducing number of MPI threads from %s to %s (number of elements in mesh)!" % (int(MPIthreads),int(nElems[0])))
+                            print(s)
+                        MPIthreads = str(min(int(nElems[0]),int(MPIthreads)))
+            except Exception as ex :
+                pass
+
 
         # check MPI built binary (only possible for reggie-compiled binaries)
         MPI_built_flag=os.path.basename(build.binary_path).upper()+"_MPI"
